51c3d068017deb4f4d58779d2c07b979344be618
Thalathai singwong <darkman@MacBook-Air.local>
2020-10-08 15:40:33 +0700

    First commit

--- database/migrations/2020_10_07_150002_create_projects_table.php
+++ database/migrations/2020_10_07_150002_create_projects_table.php
@@ -17,12 +17,12 @@ class CreateProjectsTable extends Migration
             $table->id();
             $table->text('name_th');
             $table->text('name_en');
-            $table->text('id_regStd1');
-            $table->text('id_regStd2');
-            $table->text('id_regStd3');
-            $table->text('id_director1');
-            $table->text('id_director2');
-            $table->text('id_president');
+            $table->unsignedInteger('id_regStd1');
+            $table->unsignedInteger('id_regStd2');
+            $table->unsignedInteger('id_regStd3');
+            $table->unsignedInteger('id_director1');
+            $table->unsignedInteger('id_director2');
+            $table->unsignedInteger('id_president');
             $table->text('name_mentor');
 
             $table->unsignedInteger('subject_id');
--- resources/views/system/projects.blade.php
+++ resources/views/system/projects.blade.php
@@ -17,7 +17,30 @@
      </ul>
     </div>
    @endif
-
+   <a href="" class="btn btn-primary my-2" align="left">เพิ่มโปรเจค</a>
+   <table class="table table-striped">
+      <thead>
+        <tr>
+           <th scope="col">ลำดับโครงงาน</th>
+           <th scope="col">รหัสนักศึกษา</th>
+           <th scope="col">ชื่อ-สกุล</th>
+          <th scope="col">ชื่อโครงงาน(ภาษาไทย)</th>
+          <th scope="col">ชื่อโครงงาน(ภาษาอังกฤษ)</th>
+          <th scope="col">ประธาน</th>
+          <th scope="col">กรรมการ</th>
+          <th scope="col">ที่ปรึกษาพิเศษ</th>
+          <th scope="col">หมายเหตุ</th>
+        </tr>
+      </thead>
+      <tbody>
+         <tr>
+            <th>1</th>
+            <td>6004101342 <hr> 6004101342</td>
+         </tr>
+         
+      </tbody>
+      
+    </table>
    
     
 </div>
