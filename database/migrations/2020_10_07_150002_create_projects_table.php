246c3435d357fcadbab2573bdcb0c98fc3d12d07
Thalathai02 <54341835+Thalathai02@users.noreply.github.com>
2020-11-08 23:31:49 +0700

    แก้ dataBase project_user

--- database/migrations/2020_10_07_150002_create_projects_table.php
+++ database/migrations/2020_10_07_150002_create_projects_table.php
@@ -16,21 +16,8 @@ class CreateProjectsTable extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->text('name_th');
-            $table->text('name_en');
-            $table->unsignedInteger('id_regStd1')->nullable();
-            $table->foreign('id_regStd1')->references('id')->on('reg_stds')->onDelete('cascade');
-            $table->unsignedInteger('id_regStd2')->nullable();
-            $table->foreign('id_regStd2')->references('id')->on('reg_stds')->onDelete('cascade');
-            $table->unsignedInteger('id_regStd3')->nullable();
-            $table->foreign('id_regStd3')->references('id')->on('reg_stds')->onDelete('cascade');
-            $table->unsignedInteger('id_director1')->nullable();
-            $table->foreign('id_director1')->references('id')->on('teachers')->onDelete('cascade');
-            $table->unsignedInteger('id_director2')->nullable();
-            $table->foreign('id_director2')->references('id')->on('teachers')->onDelete('cascade');
-            $table->unsignedInteger('id_president')->nullable();
-            $table->foreign('id_president')->references('id')->on('teachers')->onDelete('cascade');
-            $table->text('name_mentor')->nullable();
-
+            $table->text('name_en');            
+            $table->text('status');
             $table->unsignedInteger('subject_id')->nullable();
             $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
             $table->timestamps();
