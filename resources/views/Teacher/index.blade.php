578dc648c394d932905527dbed72d5620086dbfd
Thalathai02 <54341835+Thalathai02@users.noreply.github.com>
2021-01-30 18:49:41 +0700

    แก้ตัว Table

--- resources/views/Teacher/index.blade.php
+++ resources/views/Teacher/index.blade.php
@@ -1,66 +1,73 @@
 @extends('layouts.app')
 @section('content')
-<!DOCTYPE html>
-<html>
- <body>
-  <br />
-  
-  <div class="container">
-   <h3 align="center">Teacher information in Laravel</h3>
-    <br />
-   
+    <!DOCTYPE html>
+    <html>
 
-   @if($message = Session::get('success'))
-   <div class="alert alert-success alert-block">
-    <button type="button" class="close" data-dismiss="alert">×</button>
-           <strong>{{ $message }}</strong>
-   </div>
-   @endif
-   {!! Form::open(['action' => 'ImportExcel\ImportExcelController@Search','method'=>'POST']) !!}
-   <div class="form-group col">
-    {!! Form::label('Search','ค้นหา',["class"=>"form-group"])!!}
-    {!! Form::text('Search',null,["class"=>"form-group col-6"]) !!}
-    <input type="submit" value="ค้นหา" class="btn btn-primary  " name="" id="">
-    </div>
-    {!! Form::close() !!}
+    <body>
+        <br />
 
-        <a href="/Teacher/create" class="btn btn-primary my-2" align="left">เพิ่มข้อมูล</a>
-        
-   
-   <br />
-    <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">id</th>
-      <th scope="col">ชื่อ</th>
-      <th scope="col">อีเมล</th>
-      <th scope="col">เบอร์โทร</th>
-      <th scope="col">แก้ไข</th>
-      <th scope="col">ลบ</th>
-    </tr>
-  </thead>
-  
-  <tbody>
-    @foreach($data as $row)
-  <tr>
-    <td scope="row">{{$row->id}}</td>
-    <td>{{$row->Title_name_Instructor}}{{$row->name_Instructor}}</td>
-    <td>{{$row->email_Instructor}}</td>
-    <td>{{$row->phone_Instructor}}</td>
-    <td><a href="{{route('Teacher.edit',$row->id)}}" class="btn btn-success">แก้ไข</a></td>
-    <td>
-        <form action="{{route('Teacher.destroy',$row->id)}}" method="POST">
-            @csrf @method('DELETE')
-            <input type="submit" value="ลบ" data-name="{{$row->name}}" class="btn btn-danger deleteForm">
-        </form>
+        <div class="container">
+            <h3 align="center">Teacher information in Laravel</h3>
+            <br />
 
-      </td>
-  </tr>
-  @endforeach
-</tbody>
-</table>
-</div>
-</body>
-</html>
+
+            @if ($message = Session::get('success'))
+                <div class="alert alert-success alert-block">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    <strong>{{ $message }}</strong>
+                </div>
+            @endif
+            {!! Form::open(['action' => 'ImportExcel\ImportExcelController@Search', 'method' => 'POST']) !!}
+            <div class="form-group col">
+                {!! Form::label('Search', 'ค้นหา', ['class' => 'form-group']) !!}
+                {!! Form::text('Search', null, ['class' => 'form-group col-6']) !!}
+                <input type="submit" value="ค้นหา" class="btn btn-primary  " name="" id="">
+            </div>
+            {!! Form::close() !!}
+
+            <a href="/Teacher/create" class="btn btn-primary my-2" align="left">เพิ่มข้อมูล</a>
+
+
+            <br />
+            <div class="mb-4">
+              <div class="table-responsive table-striped">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">ชื่อ</th>
+                        <th scope="col">อีเมล</th>
+                        <th scope="col">เบอร์โทร</th>
+                        <th scope="col">แก้ไข</th>
+                        <th scope="col">ลบ</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($data as $row)
+                        <tr>
+                            <td scope="row">{{ $row->id }}</td>
+                            <td>{{ $row->Title_name_Instructor }}{{ $row->name_Instructor }}</td>
+                            <td>{{ $row->email_Instructor }}</td>
+                            <td>{{ $row->phone_Instructor }}</td>
+                            <td><a href="{{ route('Teacher.edit', $row->id) }}" class="btn btn-success">แก้ไข</a></td>
+                            <td>
+                                <form action="{{ route('Teacher.destroy', $row->id) }}" method="POST">
+                                    @csrf @method('DELETE')
+                                    <input type="submit" value="ลบ" data-name="{{ $row->name }}"
+                                        class="btn btn-danger deleteForm">
+                                </form>
+
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+          </div>
+        </div>
+        </div>
+    </body>
+
+    </html>
 
 @endsection
--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -27,42 +27,47 @@
             </div>
             {!! Form::close() !!}
             <br />
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">User</th>
-                        <th scope="col">ชื่อ</th>
-                        <th scope="col">อีเมล</th>
-                        <th scope="col">แก้ไข</th>
-                        <th scope="col">ลบ</th>
-                    </tr>
-                </thead>
-                <tbody> 
-                  @if(!empty($data) && $data->count())
-                    @foreach ($data as $row)
-                        <tr>
-                            <td scope="row">{{ $row->username }}</td>
-                            <td>{{ $row->name }}</td>
-                            <td>{{ $row->email }}</td>
-                            <td><a href="{{ route('User.edit', $row->id) }}" class="btn btn-success">แก้ไข</a></td>
-                            <td>
-                                <form action="{{ route('User.destroy', $row->id) }}" method="POST">
-                                    @csrf @method('DELETE')
-                                    <input type="submit" value="ลบ" data-name="{{ $row->name }}"
-                                        class="btn btn-danger deleteForm">
-                                </form>
+            <div class="mb-4">
+                <div class="table-responsive table-striped">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th scope="col">User</th>
+                                <th scope="col">ชื่อ</th>
+                                <th scope="col">อีเมล</th>
+                                <th scope="col">แก้ไข</th>
+                                <th scope="col">ลบ</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if (!empty($data) && $data->count())
+                                @foreach ($data as $row)
+                                    <tr>
+                                        <td scope="row">{{ $row->username }}</td>
+                                        <td>{{ $row->name }}</td>
+                                        <td>{{ $row->email }}</td>
+                                        <td><a href="{{ route('User.edit', $row->id) }}" class="btn btn-success">แก้ไข</a>
+                                        </td>
+                                        <td>
+                                            <form action="{{ route('User.destroy', $row->id) }}" method="POST">
+                                                @csrf @method('DELETE')
+                                                <input type="submit" value="ลบ" data-name="{{ $row->name }}"
+                                                    class="btn btn-danger deleteForm">
+                                            </form>
 
-                            </td>
-                        </tr>
-                    @endforeach
-                    @else
-                    <tr>
-                        <td colspan="10">There are no data.</td>
-                    </tr>
-                @endif
-                </tbody>
-            </table>
-            {!! $data->links() !!}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <tr>
+                                    <td colspan="10">There are no data.</td>
+                                </tr>
+                            @endif
+                        </tbody>
+                    </table>
+                    {!! $data->links() !!}
+                </div>
+            </div>
         </div>
     </body>
 
